6d48c75ad4a77a76e496f9184a34d3cda49ef3a2
kateko99 <kakowalczyk07@gmail.com>
2021-12-28 20:35:50 +0100

    Added column name to routes table

---
+++ BikeSite/Data/20211228193511_UpdateRoutesName.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BikeSite.Data
+{
+    public partial class UpdateRoutesName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
